c5c8b79e2f820d44a1129ee4ca1b5141bc160a75
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-25 17:46:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,7 +56,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{@owner.dogs.count}dog(s), and #{@owner.cats.count} cat(s).
+    "I have #{@owner.dogs.count}dog(s), and #{@owner.cats.count} cat(s)."
     
   end 
   
