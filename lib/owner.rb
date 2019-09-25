19260020f04ab6eb85341de028a2ae9b115f4477
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-25 17:01:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
 class Owner
-  # code goes here
+  
+  
+  attr_reader :name, :species
+  
+  @@pets = {:dogs => [], :cats => []}
+  @@all = []
+  @@count = 0
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @@count += 1
+  end
+  
+  def say_species
+   "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@count = 0
+  end 
+  
+  def cats
+    Cat.all.select { |cat| cat.owner == self }
+  end
+  
+  def dogs 
+    Dog.all.select { |dog| dog.owner == self }
+  end
+  
+  def buy_cat(name)
+   Cat.new(name)
+   
+  end
+  
+  
+  
 end
